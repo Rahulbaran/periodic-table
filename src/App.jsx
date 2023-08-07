038495369ddbd4e737733b51f6c9b0213760db67
Rahulkumar <rahulkumar109422@gmail.com>
2023-08-07 20:31:11 +0530

    ci: initialize the project with required file structures

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/index.scss";
+
+import ElementsContainer from "./components/ElementsContainer";
+
+export default function App() {
+  return (
+    <div className="container">
+      <ElementsContainer />
+    </div>
+  );
+}
---
+++ src/components/ElementsContainer.jsx
@@ -0,0 +1,9 @@
+import elementsData from "../data/elementsData";
+
+export default function ElementsContainer() {
+  return (
+    <main className="elements-wrapper">
+      <div className="elements"></div>
+    </main>
+  );
+}
